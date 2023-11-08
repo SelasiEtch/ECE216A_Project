61bb8f5919316ab4cb77fbc059ae566817aeb8c2
SelasiEtch <116213048+SelasiEtch@users.noreply.github.com>
2023-11-07 17:27:38 -0800

    Add files via upload

---
+++ M216A_TopModule.v
@@ -0,0 +1,63 @@
+`timescale 1ns / 100ps
+
+//Do NOT Modify This
+module P1_Reg_7_bit (DataIn, DataOut, rst, clk);
+
+    input [6: 0] DataIn;
+    output [6: 0] DataOut;
+    input rst;
+    input clk;
+    reg [6:0] DataReg;
+    
+    always @(posedge clk)
+        if(rst)
+            DataReg  <= 7'b0;
+        else
+            DataReg <= DataIn;
+    assign DataOut = DataReg ;          
+endmodule
+
+module P1_Reg_4_bit (DataIn, DataOut, rst, clk);
+
+    input [3: 0] DataIn;
+    output [3: 0] DataOut;
+    input rst;
+    input clk;
+    reg [3:0] DataReg;
+    
+    always @(posedge clk)
+        if(rst)
+            DataReg  <= 4'b0;
+        else
+            DataReg <= DataIn;
+    assign DataOut = DataReg ;          
+endmodule
+
+//Do NOT Modify This
+module M216A_TopModule(
+    Clk_In,
+    Width_In,
+    Height_In,
+    Index_x_Out,
+    Index_y_Out,
+    Strike,
+    Occupied_Width,
+    Rst_In);
+  input Clk_In;
+  input [3:0]Width_In;
+  input [3:0]Height_In;
+  output [6:0]Index_x_Out, Index_y_Out;
+  output [6:0]Occupied_Width[12:0];  // Connect it to a 13 element Register array
+  output [3:0]Strike;
+  input Rst_In;
+
+wire [3:0] Width_In, Height_In;
+wire Clk_In, Rst_In;
+
+//Add your code below 
+//Make sure to Register the outputs using the Register modules given above
+
+
+
+  
+endmodule
