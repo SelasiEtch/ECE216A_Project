61ec24fcde3c84cd1176c5553cdae234fb7228ab
Selasi Etchey <selasietchey@ucla.edu>
2023-11-09 15:45:05 -0800

    Building out logic for deciding where to place programs and updating occupancy. Need to update Output_x_Out and Output_y_Out as well

--- M216A_TopModule.v
+++ M216A_TopModule.v
@@ -73,30 +73,72 @@ wire Clk_In, Rst_In;
 
 //Add your code below 
 //Make sure to Register the outputs using the Register modules given above
-reg Occupied_Width_values = Occupied_Width
-reg Strip_y
-reg Strip_y_plus_1
 
-case (Height_In)
-    4'd4:
-        begin
-            Strip_y = Occuped_Width - Width_In
-            Strip_y_plus_1 = 
+reg [7:0] Occupied_Width_values [0:12];
+reg [7:0] Strip_y_occupancy
+reg [7:0] Strip_y_plus_1_occupancy
+
+always @ (posedge Clk_In or posedge Rst_In) begin
+    // Reset Occupied Width Array
+    if (Rst_In) begin
+        for (int i = 0; i < 13; i = i+1) begin
+            Occupied_Width_values[i] <= 8'b00000000
         end
-    4'd5:
-    4'd6:
-    4'd7:
-    4'd8:
-    4'd9:
-    4'd10:
-    4'd11:
-    4'd12:
-    4'd13:
-    4'd14:
-    4'd15:
-    4'd16:
+    end
+    // On positive edge of Clock, Occupied width array is updated
+    else begin
+        for (int i = 0; i < 13; i = i+1) begin
+            Occupied_Width_values[i] <= Occupied_Width[i]
+        end   
+    end
+end
 
-endcase
+always @* begin   
+    // Updating Strip Occupancy and determining placements
+    case (Height_In)
+        4'd4:
+            begin
+                Strip_y_occupancy = Occupied_Width_values[3] - Width_In;
+                Strip_y_plus_1_occupancy = Occupied_Width_values[4] - Width_In;
+                // Both strips are not filled
+                if (Strip_y_occupancy > 0 and Strip_y_plus_1_occupancy > 0) begin
+                    if (Strip_y_occupancy > Strip_y_plus_1_occupancy) begin
+                        Occupied_Width[1] = Strip_y_occupancy;
+                    end
+                    else begin
+                        Occupied_Width[1] = Strip_y_plus_1_occupancy;
+                    end
+                end
+                // Strip y i not filled
+                else if (Strip_y_occupancy > 0) begin
+                    Occupied_Width[1] = Strip_y_occupancy;
+                end
+                // Strip y+1 is not filled
+                else if (Strip_y_plus_1_occupancy > 0) begin
+                    Occupied_Width[1] = Strip_y_plus_1_occupancy;
+                end
+                // Both Strips are filled
+                else begin
+                    Strike = Strike + 1;
+                    Output_x_Out = 128;
+                    Output_y_Out = 128;
+                end
+            end
+        4'd5:
+        4'd6:
+        4'd7:
+        4'd8:
+        4'd9:
+        4'd10:
+        4'd11:
+        4'd12:
+        4'd13:
+        4'd14:
+        4'd15:
+        4'd16:
+
+    endcase
+end
 
 
   
