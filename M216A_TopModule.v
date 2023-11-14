b1bac3af82c3687ab0528904d415e2ab6e26c850
whenningson <150221523+whenningson@users.noreply.github.com>
2023-11-13 19:01:00 -0800

    Add files via upload
    Final frozen code template. Prof. changed some variable names.

---
+++ M216A_TopModule.v
@@ -0,0 +1,79 @@
+`timescale 1ns / 100ps
+
+//Do NOT Modify This
+module P1_Reg_8_bit (DataIn, DataOut, rst, clk);
+
+    input [7: 0] DataIn;
+    output [7: 0] DataOut;
+    input rst;
+    input clk;
+    reg [7:0] DataReg;
+    
+    always @(posedge clk)
+        if(rst)
+            DataReg  <= 8'b0;
+        else
+            DataReg <= DataIn;
+    assign DataOut = DataReg ;          
+endmodule
+
+module P1_Reg_5_bit (DataIn, DataOut, rst, clk);
+
+    input [4: 0] DataIn;
+    output [4: 0] DataOut;
+    input rst;
+    input clk;
+    reg [4:0] DataReg;
+    
+    always @(posedge clk)
+        if(rst)
+            DataReg  <= 5'b0;
+        else
+            DataReg <= DataIn;
+    assign DataOut = DataReg ;          
+endmodule
+
+module P1_Reg_4_bit (DataIn, DataOut, rst, clk);
+
+    input [3: 0] DataIn;
+    output [3: 0] DataOut;
+    input rst;
+    input clk;
+    reg [3:0] DataReg;
+    
+    always @(posedge clk)
+        if(rst)
+            DataReg  <= 4'b0;
+        else
+            DataReg <= DataIn;
+    assign DataOut = DataReg ;          
+endmodule
+
+//Do NOT Modify This
+module M216A_TopModule(
+    clk_i,
+    width_i,
+    height_i,
+    index_x_o,
+    index_y_o,
+    strike_o,
+    Occupied_Width,
+    rst_i);
+  input clk_i;
+  input [4:0]width_i;
+  input [4:0]height_i;
+  output [7:0]index_x_o, index_y_o;
+  output [7:0]Occupied_Width[12:0];  // Connect it to a 13 element Register array
+  output [3:0]strike_o;
+  input rst_i;
+
+wire [4:0] width_i, height_i;
+wire clk_i, rst_i;
+
+//Add your code below 
+//Make sure to Register the outputs using the Register modules given above
+
+
+
+  
+endmodule
